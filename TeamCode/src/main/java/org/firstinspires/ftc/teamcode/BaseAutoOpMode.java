e5928212a4d97a1fc44dfcfc775ec3a562c1255c
christopher_berg <cubeberg@gmail.com>
2020-09-17 17:45:00 -0400

    Adding missing files, fixing TensorFlow issue due to change from FIRST

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAutoOpMode.java
@@ -553,11 +553,8 @@ public abstract class BaseAutoOpMode extends BaseOpMode {
         // first.
         initVuforia();
 
-        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
-            initTfod();
-        } else {
-            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-        }
+        initTfod();
+
         if (tfod != null) {
             tfod.activate();
         }
