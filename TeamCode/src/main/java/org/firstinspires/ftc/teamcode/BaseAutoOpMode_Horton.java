f2e1794a298d987ed18384756948a5d671969554
Nick Horton <horotonn13@gmail.com>
2020-12-22 17:53:29 -0500

    Update BaseAutoOpMode_Horton.java
    added sleep statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAutoOpMode_Horton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAutoOpMode_Horton.java
@@ -77,6 +77,15 @@ public abstract class BaseAutoOpMode_Horton extends BaseOpMode_Horton {
 
     public void DriveDirection(Drive Direction) {
 
+        if (Direction == Drive.stop)
+            front_left.setPower(0);
+            front_right.setPower(0);
+            back_left.setPower(0);
+            back_right.setPower(0);
+            sleep(100);
+
+
+
         if (Direction == Drive.forward) {
             front_left.setPower(1);
             front_right.setPower(1);
