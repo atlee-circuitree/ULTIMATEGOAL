1ba1de6a349ca2bb626f7de729ff44ed21d08128
Sharon Hudson <sharebearhudson@gmail.com>
2020-12-22 16:36:42 -0500

    Update SystemCheckAutoHorton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SystemCheckAutoHorton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SystemCheckAutoHorton.java
@@ -5,15 +5,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @Autonomous(name = "System Check Auto", group = "Linear Opmode")
-public class SystemCheckAutoHorton extends BaseOpMode_Horton {
+public class SystemCheckAutoHorton extends BaseAutoOpMode_Horton{
 
     @Override
     public void runOpMode () {
         //Assigns hardware devices names and values
 
         GetHardware();
+        waitForStart();
+        runtime.reset();
+
+
+        telemetry.addData("status", "da robot be movin");
+        drive(STRAFE.left);
 
         telemetry.addData("status",  "Da Robot be ready" );
+
 
     }
 
