4597e3cc026798f41c03e42200b1f2c77fa9e85c
Nick Horton <horotonn13@gmail.com>
2020-12-22 16:44:01 -0500

    Update SystemCheckAutoHorton.java
    added comma

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SystemCheckAutoHorton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SystemCheckAutoHorton.java
@@ -28,7 +28,7 @@ public class SystemCheckAutoHorton extends BaseAutoOpMode_Horton{
 
         drive(STRAFE.right);
 
-        telemetry.addData("path" "complete");
+        telemetry.addData("path","complete");
         telemetry.update();
 
 
